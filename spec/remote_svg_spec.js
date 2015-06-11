d3ea295179089c3b0b0b3a7a88bb9b47e2165c9c
James Martin <james.martin@betterbills.com>
2015-06-11 07:23:42 +0100

    Play nice with no placeholders, and add class.

--- spec/remote_svg_spec.js
+++ spec/remote_svg_spec.js
@@ -14,14 +14,22 @@ JS.Test.describe('RemoteSvg', function() { with(this) {
     this.xhr.restore();
   }});
 
-  describe('loading a SVG from a remote URI', function() { with(this) {
-    it('gets the document specified in the data attribute', function() { with(this) {
+  describe('loading from a remote URI', function() { with(this) {
+    it('embeds the SVG into the placeholder of the document', function() { with(this) {
       var remoteSvg = new RemoteSvg('my-svg');
       this.requests[0].respond(200, { "Content-Type": "application/svg" },
         '<svg>some document</svg>');
 
       assertEqual(1, this.requests.length);
       assertEqual('<svg id="my-svg">some document</svg>', document.getElementById('fixture').innerHTML);
+    }});
+  }});
+
+  describe('when the placeholder does not exist', function() { with(this) {
+    it('does nothing', function() { with(this) {
+      var remoteSvg = new RemoteSvg('nonexistent');
+
+      assertEqual('', document.getElementById('my-svg').innerHTML);
     }});
   }});
 
@@ -33,6 +41,20 @@ JS.Test.describe('RemoteSvg', function() { with(this) {
         'Error!');
 
       assertEqual('<!-- SVG at "http://example.com/my-doc.svg" could not be loaded -->', document.getElementById('my-svg').innerHTML);
+    }});
+  }});
+
+  describe('transformations', function() { with(this) {
+    it('adds a class to the document', function() { with(this) {
+      var placeholder = document.getElementById('my-svg')
+      placeholder.setAttribute('data-remote-svg-class', 'some-class');
+
+      var remoteSvg = new RemoteSvg('my-svg');
+
+      this.requests[0].respond(200, { "Content-Type": "application/svg" },
+        '<svg>some document</svg>');
+
+      assertEqual('<svg id="my-svg" class="some-class">some document</svg>', document.getElementById('fixture').innerHTML);
     }});
   }});
 }});
